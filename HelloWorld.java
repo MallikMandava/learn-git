434967126ce07ddb5bd263b92a1cb362f2c94928
MallikMandava <73517859+MallikMandava@users.noreply.github.com>
2021-12-20 19:57:56 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-            System.out.println("Hello, World! Using GitHib Actions"); 
+            System.out.println(" New Hello, World! Using GitHib Actions"); 
     }
 }
